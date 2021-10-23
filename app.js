2657cb6c81a91a264b3b3de4af780d497603ad85
Pfizz <pfiste80@gmail.com>
2021-10-22 21:00:24 -0400

    added db and schema

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
+// get the client
 const inquirer = require('inquirer');
+const cTable = require('console.table');
+const mysql = require('mysql2');
 
+// create the connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Erebor@12',
+  database: 'employee_db'
+});
+
+console.log(connection);
 inquirer
   .prompt([
     {
---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS employee_db;
+
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE employee_role (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    /*  INT to hold reference to department role belongs to */
+    department_id INTEGER
+);
+
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    first_name VARCHAR(30) NOT NULL, 
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER
+    /*  INT to hold reference to another employee that
+     is manager of the current employee.
+     This field might be null if the employee has no manager. */
+);
