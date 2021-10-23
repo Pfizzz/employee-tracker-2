67a207324c8d9d7978adc69f865f4087256f3696
Pfizz <pfiste80@gmail.com>
2021-10-23 01:18:30 -0400

    get employee, role, department functional

--- app.js
+++ app.js
@@ -27,7 +27,6 @@ function startApp() {
         choices: [
           "View all employees",
           "View all employee positions",
-
           "View all departments",
           "Add department",
           "Add employee",
@@ -39,32 +38,79 @@ function startApp() {
     ])
     .then((choiceAnswers) => {
       console.log(choiceAnswers);
-      if (choiceAnswers.choices === "View all Employees") {
+      if (choiceAnswers.choices === "View all employees") {
         viewEmployees();
       }
-      if (choiceAnswers.choices === "View all roles") {
-        viewRoles();
+      if (choiceAnswers.choices === "View all employee positions") {
+        viewEmpRole();
       }
 
-      if (choiceAnswers.choices === "View Department") {
+      if (choiceAnswers.choices === "View all departments") {
         viewDepartment();
       }
-      if (choiceAnswers.choices === "Add a Department") {
+      if (choiceAnswers.choices === "Add department") {
         addDepartment();
       }
-      if (choiceAnswers.choices === "Add an Employee") {
+      if (choiceAnswers.choices === "Add employee") {
         addEmployee();
       }
-      if (choiceAnswers.choices === "Update an Employee Role") {
-        updateEmployee();
-      }
-      if (choiceAnswers.choices === "Add role") {
+      if (choiceAnswers.choices === "Add employee role") {
         addRole();
+        if (choiceAnswers.choices === "Update an employee role") {
+          updateEmployee();
+        }
       }
-      if(choiceAnswers.choices === "Exit"){
+      if (choiceAnswers.choices === "Quit application") {
         process.exit(1);
       }
     });
 }
 
+// function to view all employees
+function viewEmployees() {
+  let employeeQuery = `SELECT employee.id,
+    employee.first_name,
+    employee.last_name,
+    empRole.title,
+    employee.manager_id,
+    department.id AS 'department',
+    empRole.salary 
+    FROM employee, empRole, department
+    WHERE department.id = empRole.department_id
+    AND empRole.id = employee.role_id
+    ORDER BY employee.id ASC`;
+
+  db.promise()
+    .query(employeeQuery)
+    .then(([rows, fields]) => {
+      console.table(rows);
+      startApp();
+      
+    })
+    .catch(console.log)
+
+// function to view all employee roles
+}
+function viewEmpRole() {
+  db.query("SELECT * FROM empRole;", (err, result) => {
+    if (err) {
+      console.log("Something went wrong.");
+    }
+    console.table(result);
+    startApp();
+  });
+};
+
+// view departments
+function viewDepartment() {
+  db.query("SELECT * FROM department;", (err, result) => {
+    if (err) {
+      console.log("No departments found!");
+    }
+    console.table(result);
+    startApp();
+  });
+};
+
+// starts app on "node app"
 startApp();
