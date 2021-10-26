0399b7503f1142053e9d3c2d877bb7cafa0e45ae
Pfizz <pfiste80@gmail.com>
2021-10-25 22:28:19 -0400

    Finished challenge

--- app.js
+++ app.js
@@ -26,9 +26,8 @@ function startApp() {
         message: "Select an option from the following:",
         choices: [
           "View All Employees",
-          "View All Employees By Department",
-          "View All Employees by Manager",
-          "View all Departments",
+          "View All Departments",
+          "Add Department",
           "Add Employee",
           "Update Employee Role",
           "View all Roles",
@@ -41,15 +40,11 @@ function startApp() {
       if (choiceAnswers.choices === "View All Employees") {
         viewEmployees();
       }
-      if (choiceAnswers.choices === "View All Employees By Department") {
-        viewRole();
-      }
-      if (choiceAnswers.choices === "View All Employees by Manager") {
-        viewManager();
-      }
-      // get rid of this later
-      if (choiceAnswers.choices === "View all Departments") {
+      if (choiceAnswers.choices === "View All Departments") {
         viewDepartment();
+      }
+      if (choiceAnswers.choices === "Add Department") {
+        addDepartment();
       }
       if (choiceAnswers.choices === "Add Employee") {
         addEmployee();
@@ -68,17 +63,16 @@ function startApp() {
 
 // function to view all employees
 function viewEmployees() {
+  // JOIN managers to this
   let employeeQuery = `SELECT employee.id,
     employee.first_name,
     employee.last_name,
     role.title,
-    employee.manager_id,
-    department.id AS 'department',
+    manager.last_name AS manager,
+    department.name,
     role.salary 
-    FROM employee, role, department
-    WHERE department.id = role.department_id
-    AND role.id = employee.role_id
-    ORDER BY employee.id ASC
+    FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON department.id = role.department_id
+    LEFT JOIN employee manager ON employee.manager_id = manager.id
     `;
 
   db.promise()
@@ -93,7 +87,9 @@ function viewEmployees() {
   // function to view all employee roles
 }
 function viewRole() {
-  db.query("SELECT * FROM role;", (err, result) => {
+  // JOIN DEPARTMENT TO THIS
+  db.query(
+    "SELECT * FROM role;", (err, result) => {
     if (err) {
       console.log("Something went wrong.");
     }
@@ -102,16 +98,92 @@ function viewRole() {
   });
 };
 
-// view departments
+// view employees by departments DONE
 function viewDepartment() {
-  db.query("SELECT * FROM department;", (err, result) => {
-    if (err) {
-      console.log("No departments found!");
-    }
-    console.table(result);
-    startApp();
-  });
+  db.query(
+    "SELECT * FROM department;", (err, result) => {
+      if (err) {
+        console.log("No departments found!");
+      }
+      console.table(result);
+      startApp();
+    });
 };
+
+// add a department
+async function addDepartment() {
+  const newDepartment = await inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "Please enter the name of the new department."
+    }
+  ])
+  await db.promise().query("INSERT INTO department SET ?", newDepartment)
+  console.log("Added department");
+  startApp();
+}
+
+
+// update employee
+async function updateEmployee() {
+  const allRoles = await db.promise().query("SELECT * FROM role")
+  const allEmployees = await db.promise().query("SELECT * FROM employee")
+  const updatedRole = await inquirer.prompt([
+    {
+      type: "list",
+      name: "id",
+      message: "Please select the employee.",
+      choices: allEmployees[0].map( (employee) => ({ name: employee.last_name, value:employee.id}))
+  },
+    {
+      type: "list",
+      name: "role_id",
+      message: "Please select the employee's role.",
+      choices: allRoles[0].map( (role) => ({ name: role.title, value:role.id}))
+    }
+
+  ])
+  await db.promise().query("UPDATE employee SET role_id = ? WHERE id = ?", [updatedRole.role_id, updatedRole.id])
+  console.log("Updated employee");
+  startApp();
+}
+
+// add an eployee
+async function addEmployee() {
+  const allRoles = await db.promise().query("SELECT * FROM role")
+  const allEmployees = await db.promise().query("SELECT * FROM employee")
+  const newEmployee = await inquirer.prompt([
+    {
+      type: "input",
+      name: "first_name",
+      message: "Please enter a first name."
+    },
+    {
+    type: "input",
+    name: "last_name",
+    message: "Please enter a last name."
+  },
+  {
+    type: "list",
+    name: "role_id",
+    message: "Please select the employee's role.",
+    choices: allRoles[0].map( (role) => ({ name: role.title, value:role.id}))
+  },
+  {
+      type: "list",
+      name: "manager_id",
+      message: "Please select the employee's manager.",
+      choices: allEmployees[0].map( (employee) => ({ name: employee.last_name, value:employee.id}))
+  }
+  ]
+
+  )
+  const employeeData = await db.promise().query("INSERT INTO employee SET ?", newEmployee)
+  console.log("Added employee");
+  startApp();
+}
+
 
 // starts app on "node app"
 startApp();
